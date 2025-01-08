cea1ac440e9d404f592cfbf59f56804f5ce043b8
Rafaella Monteiro <159492173+Rafaella-Monteiro@users.noreply.github.com>
2025-01-08 19:47:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ def chat():
     return jsonify({"response": result}) #resposta retornada para  o front
 
 if __name__ == "__main__":
-    port = int(os.enviros.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
